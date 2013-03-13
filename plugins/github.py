66f166983561a867dc4bc16b42f50200a0d3f061
Thomas Boyt <me@thomasboyt.com>
2013-03-13 16:51:46 -0400

    Added no repos found message

--- plugins/github.py
+++ plugins/github.py
@@ -5,9 +5,12 @@ export_plugin = Plugin()
 @export_plugin.command(["repo", "searchrepo"])
 def search_repo(client, message, rest):
     r = requests.get("https://api.github.com/legacy/repos/search/%s" % rest)
-    repo = r.json()['repositories'][0]
-    url = "https://github.com/%s/%s" % (repo['owner'], repo['name'])
-    result = "Found GitHub repository: [%s/%s](%s)" % (repo['owner'], repo['name'], url)
+    try:
+        repo = r.json()['repositories'][0]
+        url = "https://github.com/%s/%s" % (repo['owner'], repo['name'])
+        result = "Found GitHub repository: [%s/%s](%s)" % (repo['owner'], repo['name'], url)
+    except KeyError:
+        result = "No repos found :("
 
     client.send_message({
         "type": "stream",
