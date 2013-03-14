0d4b30bef926c23d8aaee7fa46911315918c86a6
Thomas Boyt <me@thomasboyt.com>
2013-03-14 00:53:05 -0400

    Added content to !google

--- plugins/google.py
+++ plugins/google.py
@@ -9,4 +9,6 @@ def google(client, message, rest):
     url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&" + urllib.urlencode({'q': rest})
     res = json.loads(requests.get(url).text)
     res1 = res['responseData']['results'][0]
-    return "[" + res1['title'].replace('<b>', '**').replace('</b>', '**') + "](" + res1['url'] + ")"
+    title = res1['titleNoFormatting']
+    content = res1['content'].replace('<b>', '**').replace('</b>', '**')
+    return "[**%s**](%s) - %s" % (title, res1['url'], content)
